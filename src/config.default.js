e220dff268c32ea0f168c542b360a465d19008b6
Markus Muschol <markus.muschol@gmx.de>
2021-06-11 14:19:24 +0200

    Remove Firebase config

---
+++ src/config.default.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "",
+    authDomain: "ekc-stream.firebaseapp.com",
+    projectId: "ekc-stream",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+  
+  export default config;
