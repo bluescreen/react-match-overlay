89855de2ce4d9216e55763b2542e64eed95561f4
Markus Muschol <markus.muschol@gmx.de>
2021-06-12 15:10:37 +0200

    Add row click test

--- src/components/MatchTable.test.js
+++ src/components/MatchTable.test.js
@@ -1,6 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import MatchTable from './MatchTable';
 
+const fixtures = [{
+    Shiaijo: 'A',
+    NameTareWhite: 'Bob',
+    NameTareRed: 'John',
+    NumberTareWhite: 'GER-1',
+    NumberTareRed: 'BEL-1'
+},
+{
+    Shiaijo: 'B',
+    NameTareWhite: 'Max',
+    NameTareRed: 'Markus',
+    NumberTareWhite: 'GER-2',
+    NumberTareRed: 'BEL-2'
+}]
+
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useHistory: () => ({
+        push: mockHistoryPush,
+    }),
+}));
+
 describe("MatchTable", () => {
     test('renders match table', () => {
         render(<MatchTable />);
@@ -11,24 +34,24 @@ describe("MatchTable", () => {
     });
 
     test('renders match table with some data', () => {
-        const data = [{
-            NameTareWhite: 'Bob', 
-            NameTareRed: 'John', 
-            NumberTareWhite: 'GER-1', 
-            NumberTareRed: 'BEL-1'
-        },
-        {
-            NameTareWhite: 'Max', 
-            NameTareRed: 'Markus', 
-            NumberTareWhite: 'GER-2', 
-            NumberTareRed: 'BEL-2'
-        }]
 
-        const {container} = render(<MatchTable matches={data} />);
+
+        const { container } = render(<MatchTable matches={fixtures} />);
         expect(screen.getByText(/Bob/)).toBeInTheDocument();
         expect(screen.getByText(/John/)).toBeInTheDocument();
 
         expect(container.querySelectorAll('tr.match-table__row').length).toBe(2)
+    });
+
+    test('click a row', () => {
+
+
+        const { container } = render(<MatchTable matches={fixtures} />);
+
+        const firstRow = container.querySelectorAll('tr.match-table__row')[0];
+        firstRow.click()
+        expect(mockHistoryPush).toHaveBeenCalledWith('/shiaijo/A');
+
     });
 })
 
