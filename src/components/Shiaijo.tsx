07cb43c9c45ccfcfb6b4c003041f4141bce9a398
Markus Muschol <markus.muschol@gmx.de>
2021-06-15 11:29:38 +0200

    Country Flags added

--- src/components/Shiaijo.tsx
+++ src/components/Shiaijo.tsx
@@ -1,6 +1,6 @@
 import { TextConfig } from "konva/lib/shapes/Text";
 import { useEffect, useState, createRef, useRef } from "react";
-import { Stage, Layer, Text, Image, Circle,Rect } from 'react-konva';
+import { Stage, Layer, Text, Image,Rect } from 'react-konva';
 import { useResizeDetector } from 'react-resize-detector';
 
 import Match from '../models/Match'
@@ -8,6 +8,7 @@ import { TeamMatch } from "../models/TeamMatch";
 import CircleText from "./CircleTest";
 import RoundedCorner from "./RoundedCorner";
 import Ippons from "./Ippons";
+import ISO from 'iso-3166-1';
 
 const origWidth = 1200;
 //const origHeight = 540;
@@ -18,9 +19,22 @@ const Shiaijo = (props: { data: any }) => {
     const targetRef = createRef<HTMLDivElement>()
     const { width, height } = useResizeDetector({ targetRef });
     const [image, setImage] = useState()
+    const [imageFlagRed, setImageFlagRed] = useState()
+    const [imageFlagWhite, setImageFlagWhite] = useState()
     const konvaLayer = useRef<any>()
     const data = props.data;
 
+    const mapNumberToFlag = (tareNumber: string) => {
+        const countryMatch = tareNumber.match(/.[A-Z]*/) ?? []
+        const countryId:string = countryMatch[0];
+
+        const entry = ISO.whereAlpha3(countryId);
+        const entryName = countryId == 'GER' ? 'de' : entry?.alpha2.toLowerCase()
+
+        return "/flags/big/"+entryName+".gif"
+    }
+
+    /*
     data.TeamRed = "Germany"
     data.TeamWhite = "Austria"
     data.FightNumber = 5;
@@ -29,12 +43,26 @@ const Shiaijo = (props: { data: any }) => {
     data.SetRed = 4;
     data.WinsRed = 2;
     data.TeamHikiwake = 1;
+    */
+    
 
-    useEffect(() => {
+    useEffect(() => {    
         const imageObj: HTMLImageElement = new window.Image();
         imageObj.src = "/trans_overlay_1.png";
         imageObj.onload = () => {
             setImage(imageObj as any)
+        }
+
+        const flagRed: HTMLImageElement = new window.Image();
+        flagRed.src = mapNumberToFlag(data.NumberTareRed);
+        flagRed.onload = () => {
+            setImageFlagRed(flagRed as any)
+        }
+
+        const flagWhite: HTMLImageElement = new window.Image();
+        flagWhite.src = mapNumberToFlag(data.NumberTareWhite);
+        flagWhite.onload = () => {
+            setImageFlagWhite(flagWhite as any)
         }
     }, [])
 
@@ -48,7 +76,7 @@ const Shiaijo = (props: { data: any }) => {
 
     const nameOffsetX = 220 * ratioX;
     const numberOffsetX = 50 * ratioX;
-    const offsetY = 50 * ratioX;
+    const offsetY = 45 * ratioX;
 
 
     const fontSizeName = 25 * ratioX;
@@ -59,14 +87,14 @@ const Shiaijo = (props: { data: any }) => {
     const ipponPaddingX = 12 * ratioX
     const ipponBorder = 3 * ratioX
     const ipponFontSize = 22 * ratioX
-    const ippponOffsetY = 45 * ratioX;
+    const ippponOffsetY = ((data.FightNumber) ? 45 : 50) * ratioX;
     const ipponY = stageHeight - ippponOffsetY;
 
-    const pointsOffsetY = ipponY - ipponWidth -5*ratioX;
+    const pointsOffsetY = ipponY - ipponWidth - 5 * ratioX;
+    const pointsRadius = 8 * ratioX;
 
-
-    const shiaijoCircleWidth = 55*  ratioX
-    const shiaijoCirclePadding = 10*  ratioX
+    const shiaijoCircleWidth = 55 *  ratioX
+    const shiaijoCirclePadding = 10 *  ratioX
 
     const imageProps = {
         x: 0,
@@ -74,6 +102,26 @@ const Shiaijo = (props: { data: any }) => {
         image: image,
         width: width,
         height: overlayHeight
+    }
+
+    const flagWidth = 80 * ratioX;
+    const flagHeight = 40 * ratioX;
+    const flagY = stageHeight - offsetY - 90 * ratioX;
+
+    const imageFlagRedProps = {
+        x: stageWidth - numberOffsetX - flagWidth + (10 * ratioX),
+        y: flagY,
+        image: imageFlagRed,
+        width: flagWidth,
+        height: flagHeight,
+    }
+
+    const imageFlagWhiteProps = {
+        x: numberOffsetX,
+        y: flagY,
+        image: imageFlagWhite,
+        width: flagWidth,
+        height: flagHeight
     }
 
     let textNameWidth, textNumberWidth, textTeamWidth = 0;
@@ -108,6 +156,7 @@ const Shiaijo = (props: { data: any }) => {
             ...ipponParams
         }
     });
+
     const ipponsRed: TextConfig[] = [data.IpponRed1, data.IpponRed2, data.HansokuRed].filter((hit) => hit).map((hit, i) => {
         return {
             x: middle + (ipponWidth * i) + (i * ipponPaddingX) + ipponPaddingX *2,
@@ -123,7 +172,7 @@ const Shiaijo = (props: { data: any }) => {
             y: pointsOffsetY,
             text: hit,
             type: i > 0 ? 'rounded' : 'circle',
-            radius: 8 * ratioX,
+            radius: pointsRadius,
             ...ipponParams
         }
     });
@@ -133,7 +182,7 @@ const Shiaijo = (props: { data: any }) => {
             y: pointsOffsetY,
             text: hit,
             type: i > 0 ? 'rounded' : 'circle',
-            radius: 8 * ratioX,
+            radius: pointsRadius,
             ...ipponParams
         }
     });
@@ -238,6 +287,9 @@ const Shiaijo = (props: { data: any }) => {
                         height={ipponWidth}
                         border={ipponBorder}
                         radius={8 * ratioX}></RoundedCorner>}
+                    
+                    <Image {...imageFlagWhiteProps}></Image>
+                    <Image {...imageFlagRedProps}></Image>
 
                     {texts.map((textProps, i) => <Text key={i} {...textProps} ></Text>)}
                     <Ippons items={ipponsWhite} width={ipponWidth} border={ipponBorder}></Ippons>
