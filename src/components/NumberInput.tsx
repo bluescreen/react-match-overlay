00c5994ddee7473c1dc175de57a4687da43c93f1
Markus Muschol <markus.muschol@gmx.de>
2021-06-17 14:12:51 +0200

    Comact Layout

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,7 +1,7 @@
 
 const NumberInput = (props: {value: number|undefined, onChange: any}) => {
   return (
-    <input style={{width:60}}  type="number" className="form-control" value={props.value} onChange={props.onChange}></input>
+    <input style={{width:40}}  type="number" className="form-control" value={props.value} onChange={props.onChange}></input>
   )
 }
 export default NumberInput;
